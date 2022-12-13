e9c70dd7190f5c1e64d9af27e0a4b0fe9af34162
Dylan Whitehurst <nullptr@colostate.edu>
2022-12-13 19:28:27 +0000

    configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,10 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <h1 className='underline'> hello world </h1>
     </div>
   );
 }
