e6595781498b05e3a972bbf3690268aece6c339a
Dylan Whitehurst <nullptr@colostate.edu>
2022-12-14 23:35:28 +0000

    refactor

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -1,38 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-type Task = {
-    id: number,
-    label: React.Key | String | undefined | null,
-    done: Boolean
-}
+export type Task = {
+  id: number;
+  label: React.Key | String | undefined | null;
+  done: Boolean;
+};
 
-type GetTasksResponse = {
-    tasks: Task[]
+export type GetTasksResponse = {
+  tasks: Task[];
 };
 
 const Tasks = () => {
-    const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            axios.get<GetTasksResponse>('/api/tasks')
-            .then(res => {
-                if (res) setTasks(res.data.tasks);
-                
-            })
-            .catch(e => {
-                console.log(e);
-            });
-        };
-        fetchData();
-    }, []);
-    
-    return (
-        <div>
-            {tasks.map(task => (<div key={task.id} data-testid="task">{task.label}</div>))}
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    axios
+      .get<GetTasksResponse>("/api/tasks")
+      .then((res) => {
+        if (res) setTasks(res.data.tasks);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id} data-testid="task">
+          {task.label}
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Tasks;
--- client/src/unit_tests/components/Tasks.test.tsx
+++ client/src/unit_tests/components/Tasks.test.tsx
@@ -1,36 +1,39 @@
 import "@testing-library/jest-dom";
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import Tasks from "../../components/Tasks";
+import Tasks, { GetTasksResponse } from "../../components/Tasks";
 import axios from "axios";
 import { act } from "react-dom/test-utils";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-it("renders with no data", async () => {
+const setMockResponse = (responseData: GetTasksResponse) => {
   mockedAxios.get.mockResolvedValue({
-    data: {},
+    data: responseData,
+  });
+};
+
+it("renders with no tasks from response", async () => {
+  setMockResponse({ tasks: [] });
+
+  await act(async () => {
+    render(<Tasks />);
   });
 
-  render(<Tasks />);
-
-  await waitFor(() => {
-    expect(screen.queryAllByTestId("task").length).toBe(0);
-  })  
+  await wait(100);
+  expect(screen.queryAllByTestId("task").length).toBe(0);
 });
 
 it.each([
   { id: 1, label: "dishes", done: false },
   { id: 1, label: "paint", done: false },
 ])("shows label of single task given single task response", async (taskObj) => {
-  mockedAxios.get.mockResolvedValue({
-    data: { tasks: [taskObj] },
-  });
+  setMockResponse({ tasks: [taskObj] });
 
   render(<Tasks />);
 
-  
   expect((await screen.findAllByText(taskObj.label)).length).toBe(1);
   expect((await screen.findAllByTestId("task")).length).toBe(1);
 });
@@ -41,25 +44,18 @@ it.each([
       { id: 1, label: "dishes", done: false },
       { id: 2, label: "paint", done: false },
     ],
+  ],
+  [
     [
       { id: 1, label: "dishes", done: false },
       { id: 2, label: "paint", done: false },
       { id: 3, label: "pet dog", done: true },
     ],
   ],
-])(
-  "shows label of single task given single task response",
-  async (taskList) => {
-    mockedAxios.get.mockResolvedValue({
-      data: { tasks: taskList },
-    });
+])("shows label of every task in multiple task response", async (taskList) => {
+  setMockResponse({ tasks: taskList });
 
-    const tasksCUT = render(<Tasks />);
+  render(<Tasks />);
 
-    expect((await tasksCUT.findAllByTestId("task")).length).toBe(taskList.length);
-  }
-);
-
-// it.each([
-//   [{id: 1, label: "dishes", done: false }, {id: 1, label: "paint", done: false }]
-// ])("shows label of every task in multiple task response")
+  expect((await screen.findAllByTestId("task")).length).toBe(taskList.length);
+});
